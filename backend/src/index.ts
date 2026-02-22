025267f65bd0673f4414cedcb216fb64069138a4
Samanthalien004 <samantha.lien84@myhunter.cuny.edu>
2026-02-22 12:48:27 -0500

    backend connected to supabase

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { supabase } from './supabase'
 
 dotenv.config();
 
@@ -17,3 +18,14 @@ app.get("/health", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+// testing supabase connection: selects all columns from the User table and returns as json response
+app.get('/users', async (req, res) => {
+  const { data, error } = await supabase
+    .from('User')
+    .select('*')
+
+  if (error) return res.status(500).json({ error: error.message })
+  res.json(data)
+})
