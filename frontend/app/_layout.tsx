58c10e3758646b08d8e0599da685912a310a5816
juliannea <julianneaguilar74@gmail.com>
2026-02-22 12:16:55 -0500

    added auth

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,7 +2,12 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
+import 'react-native-url-polyfill/auto';
+import { useState, useEffect } from 'react';
+import { JwtPayload } from '@supabase/supabase-js';
+import { supabase } from '@/lib/supabase';
+import Auth from '@/components/Auth';
+import { View } from 'react-native';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
@@ -11,6 +16,23 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [claims, setClaims] = useState<JwtPayload | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getClaims().then(({ data }) => {
+      setClaims(data?.claims ?? null);
+    });
+
+    supabase.auth.onAuthStateChange(() => {
+      supabase.auth.getClaims().then(({ data }) => {
+        setClaims(data?.claims ?? null);
+      });
+    });
+  }, []);
+
+  if (!claims) {
+    return <Auth />;
+  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
