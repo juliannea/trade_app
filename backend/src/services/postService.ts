a9dd34939ab1fae196107f3bb155cb9c4db9e0d3
juliannea <julianneaguilar74@gmail.com>
2026-03-09 18:52:50 -0400

    refactored getPostsByCollection so that posts the user alr swiped on don't show up on feed. added getLikedPostsFromMatch so that users can view what posts they've like within a match

--- backend/src/services/postService.ts
+++ backend/src/services/postService.ts
@@ -69,7 +69,15 @@ export async function getOwnPosts(userId: string) {
 
 //GET returns all created posts of a specific collection, requires authentication 
 export async function getPostsByCollection(collectionId: number, userId: string) {
-  const { data, error } = await supabase
+  //find every post the user had already swiped on 
+  const {data: swipedPosts } = await supabase
+    .from('Swipe')
+    .select('post_id')
+    .eq('user_id', userId);
+  
+  const swipedPostIds = swipedPosts?.map(s => s.post_id) ?? [];
+
+  let query = supabase
     .from('Post')
     .select(
       `post_id, collection_id, post_title, post_image_url, post_caption,
@@ -83,6 +91,13 @@ export async function getPostsByCollection(collectionId: number, userId: string)
     //user join to get the user_name of the post creator and collection join to get the collection name for each post in the feed
     .eq('collection_id', collectionId)
     .neq('user_id', userId); //exclude the users own posts from the collection feed
+  
+  //exlude posts the user had already swiped on from the collection feed
+  if (swipedPostIds.length > 0) {
+    query = query.not('post_id', 'in', `(${swipedPostIds.join(',')})`);
+  }
+
+  const {data, error} = await query;
 
   if (error) throw new AppError(error.message, 500);
   if (!data || data.length === 0) throw new AppError('No posts found for this collection', 404);
@@ -119,4 +134,53 @@ export async function deletePost(postId: number) {
     .eq('post_id', postId);
   
   if (error) throw new AppError(error.message, 500);
+}
+
+//GET returns all the posts the the user liked from another user they're matched with 
+export async function getLikedPostsFromMatch(userId: string, matchId: number) {
+
+  //check that a match exists between the logged in user and the other user
+  const { data: match, error: matchError } = await supabase
+    .from('Match')
+    .select('user_id_a, user_id_b')
+    .eq('match_id', matchId)
+    .single();
+
+  if (matchError || !match) throw new AppError('Match not found', 404);
+  if (match.user_id_a !== userId && match.user_id_b !== userId) {
+    throw new AppError('Unauthorized', 403);
+  }
+
+  //identify the other user
+  const otherUserId = match.user_id_a === userId ? match.user_id_b : match.user_id_a;
+
+  //find all the liked posts of the other user on the Swipe table 
+  const { data: swipes, error: swipeError } = await supabase
+    .from('Swipe')
+    .select('post_id')
+    .eq('user_id', userId)
+    .eq('swipe_direction', 'RIGHT');
+
+  if (swipeError) throw new AppError(swipeError.message, 500);
+
+  //store all the liked posts in a array
+  const likedPostIds = swipes?.map(s => s.post_id) ?? [];
+  if (likedPostIds.length === 0) return [];
+
+  const { data, error } = await supabase
+    .from('Post')
+    .select(
+      `post_id, post_title, post_image_url, post_caption, user_id,
+      User!Post_user_id_fkey(  
+        user_name
+      ),
+      Collection!Post_collection_id_fkey(
+      collection_name
+      )`
+    )
+    .eq('user_id', otherUserId)
+    .in('post_id', likedPostIds);
+
+  if (error) throw new AppError(error.message, 500);
+  return data ?? [];
 }
