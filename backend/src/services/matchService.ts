beb75b25dee26e05f7e8b32a1a90a121a12b6d17
juliannea <julianneaguilar74@gmail.com>
2026-03-09 12:41:01 -0400

    added JOIN to getMatch so the username is returned

--- backend/src/services/matchService.ts
+++ backend/src/services/matchService.ts
@@ -9,7 +9,13 @@ class AppError extends Error {
 export async function getUserMatches(userId: string) {
    const {data, error} = await supabase
       .from('Match')
-      .select('match_id, matched_at, match_status, user_id_a, user_id_b')
+      .select(`match_id, matched_at, match_status, user_id_a, user_id_b,
+          user_a:User!Match_user_id_a_fkey(
+            user_name
+         ),
+         user_b:User!Match_user_id_b_fkey(
+            user_name
+         )`)
       .or(`user_id_a.eq.${userId},user_id_b.eq.${userId}`) // get all matches where the current user is either user_id_a or user_id_b
       .eq('match_status', 'ACTIVE'); // only return active matches
       
