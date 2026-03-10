1665458d32f69815622e992208aebce46ea71ab0
juliannea <julianneaguilar74@gmail.com>
2026-03-10 09:15:09 -0400

    created insertProfilePic and deleteProfilePic

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -2,8 +2,11 @@ import { Router, Response } from 'express';
 import { supabase } from '../supabase';
 import { requireAuth, AuthRequest } from '../middleware/auth';
 import * as UserController from '../controllers/userController';
+import multer from 'multer';
 
 const router = Router();
+const upload = multer({ storage: multer.memoryStorage() });
+
 
 //GET returns User's own profile information, requires authentication
 router.get('/', requireAuth, UserController.getOwnProfile);
@@ -17,6 +20,11 @@ router.patch('/', requireAuth, UserController.updateProfile);
 //========== may need to update in the future since deleting a user will also delete posts, trades, and messages ==========
 //DELETE deletes the logged in user's account, requires authentication
 router.delete('/', requireAuth, UserController.deleteAccount );
+
+//UPDATE profile picture 
+router.patch('/profile-picture', requireAuth, upload.single('image'), UserController.updateProfilePic);
+
+router.delete('/profile-picture', requireAuth, UserController.deleteProfilePic);
 
 
 export default router;
--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -11,7 +11,7 @@ class AppError extends Error {
 export async function getOwnProfile(userId: string) {
   const { data, error } = await supabase
     .from('User')
-    .select('user_id, user_name, user_first_name, user_last_name, user_email, user_phone')
+    .select('user_id, user_name, user_first_name, user_last_name, user_email, user_phone, user_profile_image')
     .eq('user_id', userId)
     .single();
 
@@ -74,4 +74,62 @@ export async function deleteAccount(userId: string) {
     .eq('user_id', userId);
 
   if (error) throw new AppError(error.message, 500);
+}
+
+//UPDATE profile picture 
+export async function updateProfilePic(userID: string, file:Express.Multer.File) {
+  //build the file parth for supabase storage 
+  const timestamp = Date.now();
+  const filePath = `${userID}/profile-picture`;
+
+  //upload the image to supabase storage 
+  const { error: uploadError } = await supabase.storage
+    .from('profile-images')
+    .upload(filePath, file.buffer, {
+      contentType: file.mimetype, //determines & sets if jpg or png
+      upsert: true, //overwrite if already exists
+    });
+  
+  if (uploadError) throw new AppError(uploadError.message, 500);
+
+  //get the public URL of the uploaded image 
+  const { data: { publicUrl } } = supabase.storage
+    .from('profile-images')
+    .getPublicUrl(filePath);
+
+  //insert in the User.user_profile_image column of logged in user 
+  const {data, error: insertError} = await supabase
+    .from('User')
+    .update({
+      user_profile_image: publicUrl
+    })
+    .eq('user_id', userID)
+    .select('user_profile_image')
+    .single();
+  
+  if (insertError) throw new AppError(insertError.message, 500);
+  return data;
+}
+
+//DELETE profile pic and change it back to null value in the column, if users don't want a picture
+export async function deleteProfilePic(userID: string) {
+  const filePath = `${userID}/profile-picture`;
+
+  //delete from supabase storage
+  const { error: storageError } = await supabase.storage
+    .from('profile-images')
+    .remove([filePath]);
+
+  if (storageError) throw new AppError(storageError.message, 500);
+
+  //set column to null
+  const { data, error } = await supabase
+    .from('User')
+    .update({ user_profile_image: null })
+    .eq('user_id', userID)
+    .select('user_profile_image')
+    .single();
+
+  if (error) throw new AppError(error.message, 500);
+  return data;
 }
