5195c498b330e6dc9b40dfcb563578e36ba08365
Samanthalien004 <samantha.lien84@myhunter.cuny.edu>
2026-04-19 20:56:17 -0400

    added pfp to return in match endpoint

--- backend/src/services/matchService.ts
+++ backend/src/services/matchService.ts
@@ -11,10 +11,10 @@ export async function getUserMatches(userId: string) {
       .from('Match')
       .select(`match_id, matched_at, match_status, user_id_a, user_id_b,
           user_a:User!Match_user_id_a_fkey(
-            user_name
+            user_name, user_profile_image
          ),
          user_b:User!Match_user_id_b_fkey(
-            user_name
+            user_name, user_profile_image
          )`)
       .or(`user_id_a.eq.${userId},user_id_b.eq.${userId}`) // get all matches where the current user is either user_id_a or user_id_b
       .eq('match_status', 'ACTIVE'); // only return active matches
